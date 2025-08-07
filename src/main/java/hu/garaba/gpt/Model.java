a2561818e2a5860229290f7ed8cb8cffc25c3e5a
tenaf0 <56382821+tenaf0@users.noreply.github.com>
2025-08-07 22:14:41 +0300

    Add experimental support for gpt-5

--- src/main/java/hu/garaba/gpt/Model.java
+++ src/main/java/hu/garaba/gpt/Model.java
@@ -5,6 +5,7 @@ public enum Model {
     O3("o3", true, true), // Smartest reasoning model
     GPT_4_1_MINI("gpt-4.1-mini", true, true), // Fast model
     GPT_4_1("gpt-4.1", true, true), // Fast model
+    GPT_5("gpt-5", true, true), // Fast model
     DALL_E_3("dall-e-3", false, false),
     WHISPER_1("whisper-1", false, false)
     ;
